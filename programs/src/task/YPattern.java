54024e09f2389ee305b6d0cbc64e16af6ff67b97
Bhavashri <158076048+Bhavashri@users.noreply.github.com>
2024-03-21 11:18:50 +0530

    YPattern.java

--- programs/src/task/YPattern.java
+++ programs/src/task/YPattern.java
@@ -4,7 +4,8 @@ public class YPattern {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		pattern(5,0,0);
+		int n=7;
+		pattern(n,0,0);
 	}
 	public static int pattern(int n,int row,int col) {
 		int res = 0;
@@ -14,13 +15,13 @@ public class YPattern {
 				if(row==col&&mid>=row&&mid>=col || col==mid&&mid<=row&&mid<=col || row+col==n-1 && mid>row) {
 				System.out.print("*");
 				}
-				res=pattern(n,row,col+1);
 				System.out.print(" ");
-				
+				res=pattern(n,row,col+1);
+				return res;	
 			}
-			res=pattern(n,row+1,col);
 			System.out.println();
-			
+			res=pattern(n,row+1,col);
+			return res;
 		}
 		return res;
 	}
